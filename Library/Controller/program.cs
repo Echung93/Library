8d6608c791a122c7bc1104bb5d046f51f11286d6
Echung93 <kty309@gmail.com>
2021-11-12 16:40:05 +0900

    2021_11_12

--- Library/Controller/program.cs
+++ Library/Controller/program.cs
@@ -11,6 +11,14 @@ public class program
 { 
     static void Main(string[] args)
     {
+        Books books = new Books();
+        Controller ct = new Controller();
+        UserRegister ur = new UserRegister();
+        List<BookVO> bookList = new List<BookVO>();
+        List<BookVO> borrowList = new List<BookVO>();
+        List<UserVO> userList = new List<UserVO>();
+        bookList = Controller.bookList(bookList);
+        userList = Controller.userList(userList);
         start start = new start();
     }
 }
